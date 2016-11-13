bab646188aade9bf39474aa399eb76814301ca4e
KimBoWoon <haioer@naver.com>
2016-11-13 23:45:35 +0900

    init

---
+++ Xamarin-Calculator/Xamarin_Calculator/App.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Calculator
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            //var content = new ContentPage
+            //{
+            //    Title = "Xamarin_Calculator",
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
+
+            //MainPage = new NavigationPage(content);
+            var content = new View.Page1
+            {
+                Title = "Hi"
+            };
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Xamarin-Calculator/Xamarin_Calculator/View/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Calculator.View
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        void btnClicked(object o, EventArgs e)
+        {
+            ((Button)o).Text = "Hello";
+        }
+    }
+}
